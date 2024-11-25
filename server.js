90da35d7acb0797f26768c9ad6c99943a9e83932
Aritra Kumar <121766655+aritrakumar@users.noreply.github.com>
2024-11-26 01:46:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ io.on('connection', (socket) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
 
